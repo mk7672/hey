909b71af0dd3e10465989b3fd419aa571d80ba5d
mk7672 <nik.k7672@gmail.com>
2025-05-08 00:01:40 +0530

    edited Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,13 +2,12 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav
-      className="text-white py-3"
-      style={{
-        background: "linear-gradient(135deg, red, black)",
-      }}
-    >
-      <div className="container mx-auto flex justify-end gap-10 px-8">
+    <nav className="bg-gradient-to-r from-red-500 to-black text-white py-3">
+      <div className="flex items-center gap-4 px-4">
+        <Link to="/"><img src="./exam-logo.jpg" alt="Blue Eyes" className="w-12 h-12 rounded-full" />
+        <div className="text-xl font-serif font-semibold">S E C</div></Link>
+      </div>
+      <div className="container mx-auto flex justify-end items-center gap-8 px-8">
         <Link to="/" className="hover:underline font-semibold">Home</Link>
         <Link to="/contact" className="hover:underline font-semibold">Contact Us</Link>
         <Link to="/login" className="hover:underline font-semibold">Login</Link>
